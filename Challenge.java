2ef8d2a47610a9a4061348d14b4730e41fa0ff1b
Ahkeem Lang <langahkeem@gmail.com>
2017-01-25 19:13:10 -0600

    Declared and initialized variables

--- Challenge.java
+++ Challenge.java
@@ -1,5 +1,13 @@
-public class Challenges {
-  public static void main(String[] args) {
-    
-  }
+import java.io.Console;
+
+ class Challenges {
+   //Declared but not initialized...
+   private int N;
+   private int K;
+
+   public Challenges(int N, K) {
+     this.N = N;
+     this.K = K;
+
+   }
 }
