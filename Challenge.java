c95b675e553491d1940476996952abe9b0002dac
Ahkeem Lang <langahkeem@gmail.com>
2017-01-25 19:36:12 -0600

    N has to be a positive integer less than 1,000,000

--- Challenge.java
+++ Challenge.java
@@ -4,10 +4,14 @@ import java.io.Console;
    //Declared but not initialized...
    private int N;
    private int K;
+   private int averageHomeSalePrice;
+
 
    public Challenges(int N, K) {
      this.N = N;
      this.K = K;
-
+   }
+   private class averageHomeSalePrice {
+     N < 1,000,000
    }
 }
